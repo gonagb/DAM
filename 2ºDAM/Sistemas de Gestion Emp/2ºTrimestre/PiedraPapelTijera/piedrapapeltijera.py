bd294f1065cd7105d617e9c6b71c3985c5b6caa3
gonagb <116161426+gonagb@users.noreply.github.com>
2024-01-30 17:17:17 +0100

    Ultimando Entregas

---
+++ 2ºDAM/Sistemas de Gestion Emp/2ºTrimestre/PiedraPapelTijera/piedrapapeltijera.py
@@ -0,0 +1,43 @@
+import random
+
+import random
+
+print("Piedra, Papel o Tijera, vO.4")
+def bucle():
+    print("1 = piedra")
+    print("2 = papel")
+    print("3 = tijera")
+    tirada = int(input("escoge tu tirada: "))
+
+    tiradaordenador = random.randint(1,3)
+
+    if tiradaordenador == 1:
+        print ("el ordenador ha sacado piedra")
+    elif tiradaordenador == 2:
+        print ("el ordenador ha sacado papel")
+    elif tiradaordenador == 3:
+        print ("el ordenador ha sacado tijera")
+
+    if tirada == 1 and tiradaordenador == 1:
+        print ("empate")
+    elif tirada == 1 and tiradaordenador == 2:
+        print ("maquina gana")
+    elif tirada == 1 and tiradaordenador == 3:
+        print ("jugador gana")
+    elif tirada == 2 and tiradaordenador == 1:
+        print ("jugador gana")
+    elif tirada == 2 and tiradaordenador == 2:
+        print ("empate")
+    elif tirada == 2 and tiradaordenador == 3:
+        print ("maquina gana")
+    elif tirada == 3 and tiradaordenador == 1:
+        print ("maquina gana")
+    elif tirada == 3 and tiradaordenador == 2:
+        print ("jugador gana")
+    elif tirada == 3 and tiradaordenador == 3:
+        print ("empate")
+    bucle()
+bucle()
+    
+    
+    
